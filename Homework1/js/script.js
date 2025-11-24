e401d8665db76818a716356badbac18296acc678
Generet234 <119113290+Generet234@users.noreply.github.com>
2025-11-24 18:41:27 +0200

    Homework1

---
+++ Homework1/js/script.js
@@ -0,0 +1,4 @@
+'use strict';
+let question = 'What is your name?';
+const name = prompt(question);//This is a built-in function that: Shows a pop-up window with an input field. Allows the user to enter text. Returns that text to the program.
+alert(`Hello, ${name} !How are you?`)//This is a built-in browser feature that shows a simple pop-up window with a message.
