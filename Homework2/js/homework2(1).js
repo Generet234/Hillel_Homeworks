b51ed0ed75936709129d84447b91bf65cd9ddba2
Generet234 <119113290+Generet234@users.noreply.github.com>
2025-11-24 18:39:00 +0200

    Delete Homework2 directory

--- Homework2/js/homework2(1).js
+++
@@ -1,6 +0,0 @@
-'use strict';
-let question = 'What is your name?';
-const name = prompt(question);
-if (confirm(name)){ // Confirm  is a built-in function that displays a dialog box with two buttons:OK or Cancel.And returns true or false.
-    alert(`Hello  ${name}, how are you?`);
-}
