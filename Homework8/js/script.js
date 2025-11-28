dfa2ee207a4483ee00d9b7c45a1f3d267a1810eb
Generet234 <119113290+Generet234@users.noreply.github.com>
2025-11-28 19:08:34 +0200

    Homework 8

---
+++ Homework8/js/script.js
@@ -0,0 +1,18 @@
+'use strict';
+const user = {
+    name: "Alex",
+    age: 25,
+    city: "Kyiv",
+    job: "Frontend"
+};
+const {name, age, city, job} = user;
+const shortInfo = {
+    name: "Alex",
+    city: "Kyiv"
+}
+const renamed = {
+    fullName: "Alex",
+    location: "Kyiv",
+    age:25,
+    job:"Frontend"
+}
