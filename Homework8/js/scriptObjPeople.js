64bb90cb4297b99533b800a120b6e0ff855e720d
Generet234 <119113290+Generet234@users.noreply.github.com>
2025-11-28 17:57:02 +0200

    Homework 8

---
+++ Homework8/js/scriptObjPeople.js
@@ -0,0 +1,14 @@
+'use strict';
+const people = [
+    {name : "Anna",age: 22},
+    {name : "Oleg",age: 31},
+    {name : "Maria",age: 27},
+];
+const firstPersonName = ["Anna", "Oleg", "Maria"];
+if (people[1].age>people[0].age || people[1].age>people[2].age) {
+    const oldest = [{firstPersonName : "Oleg", age:31}];
+}
+const ageSummary = {
+    total : people[0].age + people[1].age + people[2].age,
+    average : (people[0].age + people[1].age + people[2].age)/3
+}
